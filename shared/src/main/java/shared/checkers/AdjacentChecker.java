9c89ae7545937230c940abcf3eb8be4a9679eee8
Qirui <qh37@duke.edu>
2020-03-16 23:25:02 -0400

    checkers

---
+++ shared/src/main/java/shared/checkers/AdjacentChecker.java
@@ -0,0 +1,30 @@
+package shared.checkers;
+
+import shared.Board;
+import shared.Region;
+
+import java.util.List;
+
+public class AdjacentChecker implements Checker {
+    private Board board;
+    private Region source;
+    private Region dest;
+    private Checker next;
+
+    public AdjacentChecker(Board board, Region source, Region dest) {
+        this(board, source, dest, null);
+    }
+
+    public AdjacentChecker(Board board, Region source, Region dest, Checker next) {
+        this.source = source;
+        this.dest = dest;
+        this.next = next;
+    }
+
+    @Override
+    public boolean isValid() {
+        List<Region> neighbor = board.getNeighbor(source.getName());
+        boolean contains = neighbor.contains(dest);
+        return next == null ? contains : contains && next.isValid();
+    }
+}
