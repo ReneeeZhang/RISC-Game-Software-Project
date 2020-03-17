b1b57218ad2d252d1f4ab8273f63dbc3858dbab9
Qirui <qh37@duke.edu>
2020-03-17 09:59:26 -0400

    checkers

--- shared/src/main/java/shared/checkers/LoserChecker.java
+++ shared/src/main/java/shared/checkers/LoserChecker.java
@@ -2,16 +2,17 @@ package shared.checkers;
 
 import shared.Board;
 
-public class WinnerChecker implements Checker {
+public class LoserChecker implements  Checker {
+
     Checker next;
     Board board;
     String owner;
 
-    public WinnerChecker(Board board, String owner) {
+    public LoserChecker(Board board, String owner) {
         this(null, board, owner);
     }
 
-    public WinnerChecker(Checker next, Board board, String owner) {
+    public LoserChecker(Checker next, Board board, String owner) {
         this.next = next;
         this.board = board;
         this.owner = owner;
