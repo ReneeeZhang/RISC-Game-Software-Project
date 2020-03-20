2f4fd5620f83bb991976a9e8d8789fdb3afe06aa
Qirui <qh37@duke.edu>
2020-03-20 14:36:05 -0400

    client instruction checker

--- shared/src/main/java/shared/checkers/ClientInstructionChecker.java
+++ shared/src/main/java/shared/checkers/ClientInstructionChecker.java
@@ -2,13 +2,19 @@ package shared.checkers;
 
 import shared.*;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * ClientInstructionChecker is to check all the instructions of one client.
+ * Some instructions might be affected by former ones. Assume adjacent, accessible and
+ * quantity checks have already been conducted.
+ */
+
 public class ClientInstructionChecker implements Checker{
-    List<Move> moves;
-    List<Attack> attacks;
+    List<Instruction> instructions;
     Checker next;
 
     public ClientInstructionChecker(List<Instruction> instructions) {
@@ -16,14 +22,17 @@ public class ClientInstructionChecker implements Checker{
     }
 
     public ClientInstructionChecker(List<Instruction> instructions, Checker next) {
-        for (Instruction instruction : instructions) {
-            if (instruction instanceof Move) {
-                moves.add((Move) instruction);
+        this.instructions = instructions;
+        instructions.sort((o1, o2) -> {
+            if (!o1.getClass().equals(o2.getClass())) {
+                if (o1 instanceof Move) {
+                    return -1;
+                } else {
+                    return 1;
+                }
             }
-            if (instruction instanceof Attack) {
-                attacks.add((Attack) instruction);
-            }
-        }
+            return 0;
+        });
         this.next = next;
     }
 
@@ -31,12 +40,31 @@ public class ClientInstructionChecker implements Checker{
     @Override
     public boolean isValid() {
         Map<String, Integer> units = new HashMap<>();
-        for (Move move : moves) {
-            //TODO getSource, get
-//            String srcName = move;
+        for (Instruction instruction : instructions) {
+            R2RInstruction ins = (R2RInstruction) instruction;
+            Region src = ins.getSrc();
+            String srcName = src.getName();
+            Region dest = ins.getDest();
+            String destName = dest.getName();
+            if (!units.containsKey(srcName)) {
+                units.put(srcName, src.getNumBaseUnit());
+            }
+            if (!units.containsKey(destName)) {
+                units.put(destName, dest.getNumBaseUnit());
+            }
+            int numUnit = ins.getNumUnit();
+            int srcUnits = units.get(srcName);
+            int destUnits = units.get(destName);
+            if (srcUnits >= numUnit) {
+                units.put(srcName, srcUnits - numUnit);
+                if (ins instanceof Move) {
+                    //if move, add units to dest
+                    units.put(destName, destUnits + numUnit);
+                }
+            } else {
+                return false;
+            }
         }
-
-
-        return next.isValid();
+        return next == null || next.isValid();
     }
 }
