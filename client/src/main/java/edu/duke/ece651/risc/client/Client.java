68367b2e9b0bc7c10c4ab093db3e70c65aecc7cf
Yuanrui <zhangyuanruii@gmail.com>
2020-03-21 15:30:54 -0400

    For test purose - Change to SocketChannel

--- client/src/main/java/edu/duke/ece651/risc/client/Client.java
+++ client/src/main/java/edu/duke/ece651/risc/client/Client.java
@@ -5,9 +5,12 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.channels.SocketChannel;
 
 import shared.Board;
+import shared.GameBoard;
 import shared.Instruction;
 import shared.Move;
 
@@ -15,20 +18,16 @@ public class Client {
   private Socket s;
 
   public Client(String hostname, int port) throws IOException {
-    this.s = new Socket(hostname, port);
+    SocketChannel sc = SocketChannel.open();
+    sc.connect(new InetSocketAddress(hostname, port));
+    this.s = sc.socket();
   }
   
-  public void receiveFromServer() throws IOException {
+  public GameBoard receiveFromServer() throws IOException, ClassNotFoundException {
     DataInputStream din = new DataInputStream(s.getInputStream());
     ObjectInputStream deserial = new ObjectInputStream(din);
-    try{
-      Board b = (Board) deserial.readObject();
-      System.out.println(b);
-    } catch (ClassNotFoundException e) {
-      System.out.println(e);
-    }
+    return (GameBoard) deserial.readObject();
   }
-
   
   public void sendToServer(Instruction inst) throws IOException {
     DataOutputStream dout = new DataOutputStream(s.getOutputStream());
@@ -36,6 +35,21 @@ public class Client {
     serial.writeObject(inst);
   }
 
+  public void run() {
+    try {
+      while (true) {
+        // receive the board from GameMaster
+        //GameBoard board = receiveFromServer();
+      }
+    }
+    catch(Exception e){
+      System.out.println(e);
+    }
+  }
+
+  public void generateInst() {
+
+  }
 
   public static void main(String[] args) {
     try {
@@ -46,7 +60,7 @@ public class Client {
       Instruction inst = new Move("Fitzpatrick", "Teer", 1);
       client.sendToServer(inst);
       client.receiveFromServer();
-    } catch (IOException e) {
+    } catch (Exception e) {
       System.out.println(e);
     }
   }
