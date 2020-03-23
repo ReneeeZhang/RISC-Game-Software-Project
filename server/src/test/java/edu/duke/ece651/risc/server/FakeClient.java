3f255e6a56e658595389703c3fad4de442c7b69e
yl645 <yl645@duke.edu>
2020-03-23 18:04:52 -0400

    server test

---
+++ server/src/test/java/edu/duke/ece651/risc/server/FakeClient.java
@@ -0,0 +1,43 @@
+package edu.duke.ece651.risc.server;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import shared.*;
+
+public class FakeClient implements Runnable {
+  private Scanner sc;
+
+  public FakeClient(Scanner sc) {
+    this.sc = sc;
+  }
+  
+  public void run() {
+    try {
+      Thread.sleep(5);
+      SocketChannel socketChannel = SocketChannel.open();
+      socketChannel.connect(new InetSocketAddress("localhost", 6666));
+      Socket s = socketChannel.socket();
+      ObjectInputStream deserial = new ObjectInputStream(s.getInputStream());
+      GameBoard b = (GameBoard) deserial.readObject();
+      System.out.println(b.draw());
+      ObjectOutputStream serial = new ObjectOutputStream(s.getOutputStream());
+      List<Instruction> ins = new ArrayList<Instruction>();
+      Instruction move = new Move(sc.nextLine(), sc.nextLine(), sc.nextInt());
+      //Instruction move = new Move("Bostock", "Teer", 2);
+      ins.add(move);
+      //Instruction attack = new Attack("Teer", "Hudson", 2);
+      //ins.add(attack);
+      serial.writeObject(ins);
+    }
+    catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+}
--- server/src/test/java/edu/duke/ece651/risc/server/ServerTest.java
+++ server/src/test/java/edu/duke/ece651/risc/server/ServerTest.java
@@ -2,22 +2,17 @@ package edu.duke.ece651.risc.server;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.nio.channels.SocketChannel;
-import java.util.*;
+import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
 
-import shared.*;
-
-public class ServerTest implements Runnable {
+public class ServerTest {
   @Test
   public void test_server() {
-    Thread player1 = new Thread(new ServerTest());
-    Thread player2 = new Thread(new ServerTest());
+    Scanner sc1 = new Scanner("Bostock\nTeer\n5\n");
+    Scanner sc2 = new Scanner("Hudson\nWilson\n2\n");
+    Thread player1 = new Thread(new FakeClient(sc1));
+    Thread player2 = new Thread(new FakeClient(sc2));
     player1.start();
     player2.start();
     try{
@@ -26,27 +21,5 @@ public class ServerTest implements Runnable {
       System.out.println(e);
     }
     Server.main(new String[] { "6666" });
-  }
-
-  public void run() {
-    try {
-      Thread.sleep(5);
-      SocketChannel sc = SocketChannel.open();
-      sc.connect(new InetSocketAddress("localhost", 6666));
-      Socket s = sc.socket();
-      ObjectInputStream deserial = new ObjectInputStream(s.getInputStream());
-      GameBoard b = (GameBoard) deserial.readObject();
-      System.out.println(b.draw());
-      ObjectOutputStream serial = new ObjectOutputStream(s.getOutputStream());
-      List<Instruction> ins = new ArrayList<Instruction>();
-      Instruction move = new Move("Bostock", "Teer", 2);
-      ins.add(move);
-      //Instruction attack = new Attack("Teer", "Hudson", 2);
-      //ins.add(attack);
-      serial.writeObject(ins);
-    }
-    catch (Exception e) {
-      System.out.println(e);
-    }
   }
 }
