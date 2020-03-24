145aafa1972a0d0b21779db453d3607fe7ca9182
yl645 <yl645@duke.edu>
2020-03-23 21:08:42 -0400

    fakeserver in client  test

--- client/src/test/java/edu/duke/ece651/risc/client/ClientTest.java
+++ client/src/test/java/edu/duke/ece651/risc/client/ClientTest.java
@@ -2,41 +2,19 @@ package edu.duke.ece651.risc.client;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
-import shared.*;
 
 import org.junit.jupiter.api.Test;
 
-public class ClientTest implements Runnable{
+public class ClientTest {
   @Test
   public void test_client() {
-    Thread serverThread = new Thread(new ClientTest());
+    Thread serverThread = new Thread(new FakeServer());
     serverThread.start();
     try{
-      System.setIn(new FileInputStream("src/resource/ClientTest.txt"));
+      System.setIn(new FileInputStream("src/test/resources/ClientTest.txt"));
     } catch (FileNotFoundException e) {
       System.out.println(e);
     }
     Client.main(new String[] { "localhost", "6666" });
-  }
-
-  public void run() {
-    try{
-      ServerSocketChannel ssc = ServerSocketChannel.open();
-      ssc.socket().bind(new InetSocketAddress(6666));
-      SocketChannel sc = ssc.accept();
-      Socket s = sc.socket();
-      ObjectOutputStream serial = new ObjectOutputStream(s.getOutputStream());
-      serial.writeObject(new GameBoard());
-    } catch (IOException e) {
-      System.out.println(e);
-    }
-    
   }
 }
