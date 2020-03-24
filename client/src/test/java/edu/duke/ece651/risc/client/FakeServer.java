7654dba0768a19a5d04047295721254e177aea9a
yl645 <yl645@duke.edu>
2020-03-23 23:58:17 -0400

    client test

--- client/src/test/java/edu/duke/ece651/risc/client/FakeServer.java
+++ client/src/test/java/edu/duke/ece651/risc/client/FakeServer.java
@@ -1,13 +1,15 @@
 package edu.duke.ece651.risc.client;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 
-import shared.GameBoard;
+import shared.Board;
+import shared.Initializer;
 
 public class FakeServer implements Runnable {
   public void run() {
@@ -17,7 +19,35 @@ public class FakeServer implements Runnable {
       SocketChannel sc = ssc.accept();
       Socket s = sc.socket();
       ObjectOutputStream serial = new ObjectOutputStream(s.getOutputStream());
-      serial.writeObject(new GameBoard());
+      serial.writeObject("Player1");
+      Initializer init = new Initializer(2);
+      Board board = init.initGame();
+      serial = new ObjectOutputStream(s.getOutputStream());
+      serial.writeObject(board);
+      ObjectInputStream deserial = new ObjectInputStream(s.getInputStream());
+      board.move("Bostock", "Teer", 5);
+      board.attack("Teer", "Fitzpatrick", 10);
+      board.move("Hudson", "Wilson", 2);
+      board.attack("Wilson", "Bostock", 5);
+      board.resolve();
+      serial = new ObjectOutputStream(s.getOutputStream());
+      serial.writeObject(board);
+      deserial = new ObjectInputStream(s.getInputStream());
+      board.move("Fitzpatrick", "Bostock", 5);
+      board.attack("Fitzpatrick", "Perkins", 1);
+      board.move("Perkins", "Teer", 6);
+      board.attack("Teer", "Bostock", 7);
+      board.resolve();
+      serial = new ObjectOutputStream(s.getOutputStream());
+      serial.writeObject(board);
+      deserial = new ObjectInputStream(s.getInputStream());
+      board.move("Hudson", "Wilson", 1);
+      board.attack("Bostock", "Teer", 10);
+      board.move("Teer", "Teer", 0);
+      board.attack("Teer", "Bostock", 1);
+      board.resolve();
+      serial = new ObjectOutputStream(s.getOutputStream());
+      serial.writeObject(board);
     } catch (IOException e) {
       System.out.println(e);
     }
--- shared/src/test/java/shared/GameBoardTest.java
+++ shared/src/test/java/shared/GameBoardTest.java
@@ -1,5 +1,6 @@
 package shared;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
@@ -10,6 +11,17 @@ public class GameBoardTest {
   public void test_GameBoard() {
     Board b1 = new GameBoard();
     Board b2 = new GameBoard(new HashMap<Region, List<Region>>());
+    Initializer init = new Initializer(5);
+    try{
+      Board board = init.initGame();
+      board.getAllOwners();
+      board.getAllRegionNames();
+      board.getAllRegions();
+      board.getNeighbor("Hudson");
+      board.getRegion("Hudson");
+    } catch (IOException e) {
+      System.out.println(e);
+    }
   }
 
 }
