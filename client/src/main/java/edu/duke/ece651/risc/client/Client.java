8c9eeb287a639be3059b33e7fc674bfd1396d35c
fh65 <fh65@duke.edu>
2020-04-03 20:21:51 -0400

    try game view

--- client/src/main/java/edu/duke/ece651/risc/client/Client.java
+++ client/src/main/java/edu/duke/ece651/risc/client/Client.java
@@ -3,15 +3,17 @@ package edu.duke.ece651.risc.client;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Client extends Application {
 
-  // Button button;
   Stage window;
-  //Scene scene1, scene2;
 
   public static void main(String[] args) {
     launch(args);
@@ -19,36 +21,6 @@ public class Client extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-
-    /* TOOL2 ===== switch scene */
-    // window = primaryStage;
-
-    // Label label1 = new Label("first scene");
-    // Button button1 = new Button("go to scene2");
-    // button1.setOnAction(e -> window.setScene(scene2));
-
-    // //Layout 1 - childrean laid out in vertial column
-    // VBox layout1 = new VBox(20);
-    // layout1.getChildren().addAll(label1, button1);
-    // scene1 = new Scene(layout1, 200, 200);
-
-    // //Button2
-    // Button button2 = new Button("go to scene1");
-    // button2.setOnAction(e -> window.setScene(scene1));
-
-    // Button button3 = new Button("go to scene3");
-    
-
-    // // Layout 2
-    // StackPane layout2 = new StackPane();
-    // layout2.getChildren().add(button2);
-    // scene2 = new Scene(layout2, 600, 300);
-
-    // window.setScene(scene1);
-    // window.setTitle("Title");
-    // window.show();
-
-    /*tool3 ===== menu */
     window = primaryStage;
     window.setTitle("RISC");
 
@@ -58,10 +30,29 @@ public class Client extends Application {
     Button button3 = new Button("Room3");
     roomChange.getChildren().addAll(button1, button2, button3);
 
+    HBox insChange = new HBox();
+    Button button4 = new Button("Move");
+    Button button5 = new Button("Attack");
+    Button button6 = new Button("Upgrade");
+    insChange.getChildren().addAll(button4, button5, button6);
+    
+    Label sourceLabel = new Label("Source");
+    TextField sourceInput = new TextField();
+    Label destLabel = new Label("Destination");
+    TextField destInput = new TextField();
+
+    Button actionButton = new Button("Add action");
+    Button doneButton = new Button("Done");
+
+    VBox allIns = new VBox();
+    allIns.getChildren().addAll(insChange, sourceLabel, sourceInput, destLabel, destInput, actionButton, doneButton);
+    
+
     BorderPane borderPane = new BorderPane();
     borderPane.setTop(roomChange);
+    borderPane.setRight(allIns);
 
-    Scene scene = new Scene(borderPane, 300, 250);
+    Scene scene = new Scene(borderPane, 600, 400);
     window.setScene(scene);
     window.show();
   }
