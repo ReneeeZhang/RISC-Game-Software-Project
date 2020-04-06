c9078a919780cccafa4dadc6206b99f90c5274e8
fh65 <fh65@duke.edu>
2020-04-05 21:17:18 -0400

    modify move/attack

--- shared/src/main/java/shared/instructions/Attack.java
+++ shared/src/main/java/shared/instructions/Attack.java
@@ -7,13 +7,13 @@ import shared.checkers.*;
 public class Attack extends R2RInstruction implements Serializable{
   private static final long serialVersionUID = 923749346;
   
-  public Attack(String s, String d, int n) {
-    super(s, d, n);
+  public Attack(String s, String d, int l, int n) {
+    super(s, d, l, n);
   }
 
   @Override
   public void execute(Board b) {
-    b.attack(src, dest, numUnit);
+    b.attack(src, dest, level, numUnit);
   }
 
   @Override
--- shared/src/main/java/shared/instructions/R2RInstruction.java
+++ shared/src/main/java/shared/instructions/R2RInstruction.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 abstract public class R2RInstruction implements Instruction, Serializable {
   protected String src;
   protected String dest;
+  protected int level;
   protected int numUnit;
   private static final long serialVersionUID = 435352123;
 
@@ -13,9 +14,10 @@ abstract public class R2RInstruction implements Instruction, Serializable {
   }
 
   // Constructor
-  public R2RInstruction(String s, String d, int n) {
+  public R2RInstruction(String s, String d, int l, int n) {
     this.src = s;
     this.dest = d;
+    this.level = l;
     this.numUnit = n;
   }
   
