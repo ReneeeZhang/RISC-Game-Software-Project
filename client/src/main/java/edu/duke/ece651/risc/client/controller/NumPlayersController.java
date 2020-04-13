06fadbc0a2b904b4a149d427fe15067da9aa5f61
fh65 <fh65@duke.edu>
2020-04-12 20:34:57 -0400

    fix bug in fxml

---
+++ client/src/main/java/edu/duke/ece651/risc/client/controller/NumPlayersController.java
@@ -0,0 +1,27 @@
+package edu.duke.ece651.risc.client.controller;
+
+import edu.duke.ece651.risc.client.*;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NumPlayersController {
+
+  private ClientGUI gui;
+  
+  public NumPlayersController(ClientGUI g) {
+    this.gui = g;
+  }
+
+  @FXML
+  public void selectNum() {
+
+  }
+
+}
