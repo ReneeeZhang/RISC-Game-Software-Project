d24f2186d630b41fd6545c63d37824fb0ff1d6bf
Yuanrui <zhangyuanruii@gmail.com>
2020-04-22 00:18:07 -0400

    For chat room

---
+++ client/src/main/java/edu/duke/ece651/risc/client/ChatController.java
@@ -0,0 +1,7 @@
+package edu.duke.ece651.risc.client;
+public class ChatController extends Connector {
+
+  public ChatController(String hostname, int chatPort) {
+    super(hostname, chatPort);
+  }
+}
