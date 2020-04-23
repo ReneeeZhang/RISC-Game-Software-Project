7205b97155ce2757c2502878e56b1601ab0cda90
yl645 <yl645@duke.edu>
2020-04-22 23:20:05 -0400

    server test

--- server/src/test/java/edu/duke/ece651/risc/server/ServerTest.java
+++ server/src/test/java/edu/duke/ece651/risc/server/ServerTest.java
@@ -6,20 +6,23 @@ public class ServerTest {
   @Test
   public void test_Server() {
     try {
+      // start fake clients
       Thread fc1 = new Thread(new FakeClient("Player1\nHudson\nWilson\n0\n1\nPlayer1\nWilson\nBostock\n0\n2\n"));
       Thread fc2 = new Thread(new FakeClient("Player2\nTeer\nBostock\n0\n1\nPlayer2\nPerkins\nFitzpatrick\n0\n1\n"));
-      //Thread fc3 = new Thread(new FakeClient(""));
-      //Thread fc4 = new Thread(new FakeClient(""));
+      Thread fc3 = new Thread(new FakeClient(""));
       fc1.start();
       fc2.start();
-      //fc3.start();
-      //fc4.start();
+      fc3.start();
+
+      // runserver
       Server server = Server.start("src/main/resources/config.txt");
-      for (int i = 0; i < 2; i++) {
+      for (int i = 0; i < 3; i++) {
         server.handleRequest();
       }
-      System.out.println("success");
-      Thread.sleep(5000);
+
+      // wait for gamemaster and clients to stop
+      fc1.join();
+      fc2.join();
     } catch (Exception e) {
     }
   }
