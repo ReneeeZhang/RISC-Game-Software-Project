717975452b9180a9b6c16cea03cdcb1c6b1b2e03
Qirui <qh37@duke.edu>
2020-04-26 02:08:10 -0400

    chatcontroller

---
+++ client/src/main/java/edu/duke/ece651/risc/client/ChatController.java
@@ -0,0 +1,8 @@
+package edu.duke.ece651.risc.client;
+
+public class ChatController extends Connector {
+
+    public ChatController(String hostname, int port) {
+        super(hostname, port);
+    }
+}
