3e7ccb5e2c5537314c507040d5628dd3edf2d31f
Yuanrui <zhangyuanruii@gmail.com>
2020-04-25 23:00:29 -0400

    add incite defection test

---
+++ shared/src/test/java/shared/instructions/InciteDefectionTest.java
@@ -0,0 +1,31 @@
+package shared.instructions;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import shared.Board;
+import shared.Initializer;
+
+public class InciteDefectionTest {
+  private Board board;
+
+  @BeforeEach
+  public void init() {
+    try{
+    Initializer init = new Initializer(2);
+    this.board = init.initGame();
+  } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+  
+  @Test
+  public void test_isValid() {
+    InciteDefection id = new InciteDefection(null, "Perkins", "Fitzpatrick", 0, 0);
+    assertTrue(id.isValid(board));
+  }
+}
