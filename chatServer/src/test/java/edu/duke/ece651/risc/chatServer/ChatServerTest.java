9069c94a72c4c65eaff90b5334804cc3326e41bc
yl645 <yl645@duke.edu>
2020-04-27 23:46:31 -0400

    chat server test

--- chatServer/src/test/java/edu/duke/ece651/risc/chatServer/ChatServerTest.java
+++ chatServer/src/test/java/edu/duke/ece651/risc/chatServer/ChatServerTest.java
@@ -1,7 +1,5 @@
 package edu.duke.ece651.risc.chatServer;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 public class ChatServerTest {
@@ -12,17 +10,17 @@ public class ChatServerTest {
       Thread fakeClient2 = new Thread(new FakeClient("mesg2"));
       fakeClient1.start();
       fakeClient2.start();
-      
+
       ChatServer server = ChatServer.start("src/main/resources/config.txt");
       for (int i = 0; i < 2; i++) {
         server.handleRequest();
       }
-      
-      fakeClient1.join();
-      fakeClient2.join();
-      
+
       Thread fakeClient3 = new Thread(new FakeClient(""));
       fakeClient3.start();
+
+      fakeClient1.join();
+      fakeClient2.join();
       
       server.handleRequest();
     } catch (Exception e) {
